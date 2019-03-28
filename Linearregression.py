3cd27ac1484cecc2aa3002a75a9232316f55d93f
kathy908000 <44422100+kathy908000@users.noreply.github.com>
2019-03-28 16:14:21 -0400

    Add files via upload

---
+++ Linearregression.py
@@ -0,0 +1,30 @@
+#loss function for m and b approach 0
+#y=mx+b
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+
+FILENAME = "newiris.csv"
+HEADERS = "a" ,"b" 
+
+df = pd.read_csv(FILENAME, names=HEADERS)
+print(df.sample(8))
+
+X=df['b']
+y=df['c']
+
+print("done")
+
+def linear_regression(X, y, m_current=0, b_current=0, epochs=20, learning_rate=0.0001):
+     N = float(len(y))
+     print(done)
+     for i in range(epochs):
+          y_current = (m_current * X) + b_current
+          cost = sum([data**2 for data in (y-y_current)]) / N
+          m_gradient = -(2/N) * sum(X * (y - y_current))
+          b_gradient = -(2/N) * sum(y - y_current)
+          m_current = m_current - (learning_rate * m_gradient)
+          b_current = b_current - (learning_rate * b_gradient)
+     print(m_current, b_current, cost)
+     print("done")
